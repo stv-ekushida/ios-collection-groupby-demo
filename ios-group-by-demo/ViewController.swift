82be63392ae199d511ffd6bb8ab95b4c278d41e0
e.kushida <e.kushida@st-ventures.jp>
2017-06-20 11:40:57 +0900

    ios-collection-group-by-demo

--- ios-group-by-demo/ViewController.swift
+++ ios-group-by-demo/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let days = [
+        "2017/10/10 10:10:10",
+        "2017/10/10 10:10:10",
+        "2017/10/09 10:10:10",
+        "2017/10/09 10:10:10",
+        "2017/10/10 10:10:10",
+        "2017/10/08 10:10:10",
+        "2017/10/08 10:10:10",
+        "2017/10/10 10:10:10",
+        ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //使い方
+        let result = days.groupBy {
+
+            //グルーピングするキー
+            $0.components(separatedBy: " ").first!
+        }
+
+        _ = result.map {
+            print($0.key)
+        }
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+//Group byするExtension
+extension Sequence {
+
+    func groupBy<G: Hashable>(closure: (Iterator.Element)->G) -> [G: [Iterator.Element]] {
+        var results = [G: Array<Iterator.Element>]()
+
+        forEach {
+            let key = closure($0)
+
+            if var array = results[key] {
+                array.append($0)
+                results[key] = array
+            }
+            else {
+                results[key] = [$0]
+            }
+        }
+        
+        return results
     }
-
-
 }
 
